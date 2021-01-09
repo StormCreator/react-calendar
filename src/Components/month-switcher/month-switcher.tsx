410b03c42cc7e3196ab269399dee0352321fd160
MarginToppson <insetml@gmail.com>
2021-01-09 13:31:45 +0200

    Added month-switcher

--- src/Components/month-switcher/month-switcher.tsx
+++ src/Components/month-switcher/month-switcher.tsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React from 'react';
+import './monthSwitcher.css';
+import { Component } from 'react';
+import arrowPrev from '../../assets/img/arrow-prev.svg';
+import arrowNext from '../../assets/img/arrow-next.svg';
 
-
-const MonthSwitcher = () => {
-    return (
-        <thead>
-            <tr>
-                <td>
-                    <p>
-                        Month Switcher HERE!
-                    </p>
-                </td>
-            </tr>
-        </thead>
-    )
+export interface MonthSwitcherProps{
+    dateChanger: Function
+    monthName: string
 }
+
+class MonthSwitcher extends Component<MonthSwitcherProps> {
+
+    constructor(props: MonthSwitcherProps){
+        super(props);
+    }
+
+    render(){
+        return(
+            <thead>
+                <tr className="month-switcher">
+                    <td>
+                        <button onClick = {() => this.props.dateChanger(-1)}>
+                            <img src={arrowPrev} alt="prev-arrow-icon"/>
+                        </button>
+                        <span>{this.props.monthName}</span>
+                        <button onClick = {() => this.props.dateChanger(1)}>
+                            <img src={arrowNext} alt="nex-arrow-icon"/>
+                        </button>
+                    </td>
+                </tr>
+            </thead>
+        )
+    }
+
+
+   
+} 
 
 export default MonthSwitcher;
--- src/Components/table-layout/table-layout.tsx
+++ src/Components/table-layout/table-layout.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { Component } from "react";
+import './tableLayout.css';
 import MonthSwitcher from '../month-switcher';
+import { add, format } from 'date-fns';
 
+interface TableState{
+    currentDate: Date
+}
 
-class TableLayout extends Component{
+class TableLayout extends Component<{}, TableState>{
+
+    constructor(props: any){
+        super(props);
+        this.state = {
+            currentDate:  new Date()
+        }
+
+        this.switchMonth = this.switchMonth.bind(this);
+    }
 
     render(){
         return(
             <table>
-                <MonthSwitcher />
+                <MonthSwitcher 
+                    dateChanger = {this.switchMonth}
+                    monthName = {format(this.state.currentDate, 'MMMM')}
+                />
             </table>
         );
     }
+
+    protected switchMonth(switchDirection: number): void{
+        this.setState({
+            currentDate: add(this.state.currentDate   , {  months: switchDirection })
+
+
+        });
+    }
+
 }
 
 export default TableLayout;
