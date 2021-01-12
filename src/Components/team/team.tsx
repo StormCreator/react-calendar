ceb39446606abe066cac6ab82802200fa7a431a7
Miroslav Strukov <miroslavstrukov3@gmail.com>
2021-01-12 13:42:27 +0200

    added hide

--- src/Components/team/team.tsx
+++ src/Components/team/team.tsx
@@ -3,33 +3,39 @@ import React from "react";
 import { Day } from "../../utils/Day";
 import { User } from "../../utils/user";
 import { Vacation } from "../../utils/vacation";
+import './teamTable.css';
 
 interface TeamProps{
     key:number;
     name:string;
     users:User[];
-    retracted:boolean;
     currentDate:Date;
-    switchVisibility():void;
     vacations:Vacation[];
     daysInMonth:Day[];
 }
 
 class TeamTable extends React.Component<TeamProps,any>{
-    
+    state={
+        retracted:false
+    }
+    switchVisibility=()=>{
+        this.setState({retracted:!this.state.retracted});
+    }
     render(){
-        return (<tbody>
+        return (<tbody className={this.state.retracted?" retracted":''}>
             <tr key={'tr1'}>
                 <th>
                 {
                     this.props.name
                 }
+                <span> users:{this.props.users.length}</span>
+                <button onClick={this.switchVisibility}>hide</button>
                 </th>
                 {
                     this.props.daysInMonth.map((item)=>{
                         return(
-                            <td className={'daycell'} key={item.date.toString()}>
-                                {'dffsdfgsd'}
+                            <td className={`daycell ${item.isWeekend?'weekend':''}`} key={item.date.toString()}>
+                                {''}
                             </td>
                         )
                     })
@@ -37,13 +43,13 @@ class TeamTable extends React.Component<TeamProps,any>{
             </tr >
                 {
                     this.props.users.map((user)=>{
-                        return ( <tr key={user.id+'tr'}>
+                        return ( <tr key={user.id+'tr'} className={'userRow'}>
                             <th key={user.id}>{user.name}</th>
                         {
                             this.props.daysInMonth.map((item)=>{
                                 return(
-                                    <td key={item.date.toString()}>
-                                        {'Hello'}
+                                    <td key={item.date.toString()} className={`daycell ${item.isWeekend?'weekend':''}`}>
+                                        {''}
                                     </td>
                                 )
                             })
